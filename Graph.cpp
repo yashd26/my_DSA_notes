1848946e31cda3bf99a79ce6ac755b857440903e
yashwho <yashd026@gmail.com>
2022-09-01 17:08:03 +0530

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -222,3 +222,31 @@ void main() {
 
     print(t);
 }
+
+//Topological sort
+void findTopoSort(int node, vector<int> &vis, stack<int> &st, vector<int> adj[]) {
+    vis[node] = 1;
+    
+    for(auto it: adj[node]) {
+        if (!vis[it]) {
+            findTopoSort(it, vis, st, adj);
+        }
+    }
+    st.push(node);
+}
+
+vector<int> topoSort(int N, vector<int> adj[]) {
+    stack<int> st;
+    vector<int> vis(N, 0);
+    for(int i = 0; i < N; ++i) {
+        if (vis[i] == 0) {
+            findTopoSort(i, vis, st, adj);
+        }
+    }
+    vector<int> topo;
+    while(!st.empty()) {
+        topo.push_back(st.top());
+        st.pop();
+    }
+    return topo;
+}
